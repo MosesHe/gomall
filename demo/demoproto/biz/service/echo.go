b8f521d743375f63de0d78ff3cd8117b79ae4b0c
MosesHe <h394421599@gmail.com>
2025-02-22 12:22:40 +0800

    feat: Add metainfo and error demo

--- demo/demoproto/biz/service/echo.go
+++ demo/demoproto/biz/service/echo.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	pbapi "github.com/MosesHe/gomall/demo/demoproto/kitex_gen/pbapi"
+	"github.com/bytedance/gopkg/cloud/metainfo"
+	"github.com/cloudwego/kitex/pkg/kerrors"
 )
 
 type EchoService struct {
@@ -16,6 +19,10 @@ func NewEchoService(ctx context.Context) *EchoService {
 // Run create note info
 func (s *EchoService) Run(req *pbapi.Request) (resp *pbapi.Response, err error) {
 	// Finish your business logic.
-
+	clientName, ok := metainfo.GetPersistentValue(s.ctx, "CLIENT_NAME")
+	fmt.Println(clientName, ok)
+	if req.Message == "error" {
+		return nil, kerrors.NewGRPCBizStatusError(1004001, "client_param_error")
+	}
 	return &pbapi.Response{Message: req.Message}, nil
 }
--- demo/demoproto/cmd/client/main.go
+++ demo/demoproto/cmd/client/main.go
@@ -2,28 +2,42 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 
 	"github.com/MosesHe/gomall/demo/demoproto/kitex_gen/pbapi"
 	"github.com/MosesHe/gomall/demo/demoproto/kitex_gen/pbapi/echoservice"
+	"github.com/bytedance/gopkg/cloud/metainfo"
 	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/kerrors"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/transmeta"
+	"github.com/cloudwego/kitex/transport"
 	consul "github.com/kitex-contrib/registry-consul"
 )
 
 func main() {
-
 	r, err := consul.NewConsulResolver("127.0.0.1:8500")
 	if err != nil {
 		log.Fatal(err)
 	}
-	c, err := echoservice.NewClient("demoproto", client.WithResolver(r))
+	c, err := echoservice.NewClient("demoproto", client.WithResolver(r),
+		client.WithTransportProtocol(transport.GRPC),
+		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	res, err := c.Echo(context.TODO(), &pbapi.Request{Message: "hello"})
+	ctx := metainfo.WithPersistentValue(context.Background(), "CLIENT_NAME", "demoproto_client")
+	res, err := c.Echo(ctx, &pbapi.Request{Message: "error"})
+	var bizErr *kerrors.GRPCBizStatusError
 	if err != nil {
-		log.Fatal(err)
+		ok := errors.As(err, &bizErr)
+		if ok {
+			fmt.Printf("%#v", bizErr)
+		}
+		klog.Fatal(err)
 	}
 	fmt.Printf("%v", res)
 }
