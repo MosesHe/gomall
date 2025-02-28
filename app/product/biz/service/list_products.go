b029d54a54fbf655fef6de13560ad9ec5fb64b0a
MosesHe <h394421599@gmail.com>
2025-02-28 17:55:48 +0800

    feat: Add list_products

--- app/product/biz/service/list_products.go
+++ app/product/biz/service/list_products.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+
+	"github.com/MosesHe/gomall/app/product/biz/dal/mysql"
+	"github.com/MosesHe/gomall/app/product/biz/model"
 	product "github.com/MosesHe/gomall/rpc_gen/kitex_gen/product"
 )
 
@@ -15,6 +18,21 @@ func NewListProductsService(ctx context.Context) *ListProductsService {
 // Run create note info
 func (s *ListProductsService) Run(req *product.ListProductsReq) (resp *product.ListProductsResp, err error) {
 	// Finish your business logic.
+	categoryQuery := model.NewCategoryQuery(s.ctx, mysql.DB)
 
-	return
+	c, err := categoryQuery.GetProductsByCategoryName(req.CategoryName)
+	resp = &product.ListProductsResp{}
+	for _, v := range c {
+		for _, p := range v.Products {
+			resp.Products = append(resp.Products, &product.Product{
+				Id:          uint32(p.ID),
+				Picture:     p.Picture,
+				Price:       p.Price,
+				Description: p.Description,
+				Name:        p.Name,
+			})
+		}
+	}
+
+	return resp, nil
 }
