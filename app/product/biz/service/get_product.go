64179cf525951a1d3edd92deb84df40735bf3fd5
MosesHe <h394421599@gmail.com>
2025-02-28 14:18:24 +0800

    feat: Add get_product

--- app/product/biz/service/get_product.go
+++ app/product/biz/service/get_product.go
@@ -2,7 +2,11 @@ package service
 
 import (
 	"context"
+
+	"github.com/MosesHe/gomall/app/product/biz/dal/mysql"
+	"github.com/MosesHe/gomall/app/product/biz/model"
 	product "github.com/MosesHe/gomall/rpc_gen/kitex_gen/product"
+	"github.com/cloudwego/kitex/pkg/kerrors"
 )
 
 type GetProductService struct {
@@ -14,7 +18,24 @@ func NewGetProductService(ctx context.Context) *GetProductService {
 
 // Run create note info
 func (s *GetProductService) Run(req *product.GetProductReq) (resp *product.GetProductResp, err error) {
-	// Finish your business logic.
+	if req.Id == 0 {
+		return nil, kerrors.NewGRPCBizStatusError(2004001, "product id is required")
+	}
 
-	return
+	productQuery := model.NewProductQuery(s.ctx, mysql.DB)
+
+	p, err := productQuery.GetById(int(req.Id))
+	if err != nil {
+		return nil, err
+	}
+
+	return &product.GetProductResp{
+		Product: &product.Product{
+			Id:          uint32(p.ID),
+			Picture:     p.Picture,
+			Price:       p.Price,
+			Description: p.Description,
+			Name:        p.Name,
+		},
+	}, nil
 }
