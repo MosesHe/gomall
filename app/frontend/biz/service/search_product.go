b2bd24c36a5503cc02c7453fe9d583de5d23d55a
MosesHe <h394421599@gmail.com>
2025-03-01 21:11:56 +0800

    feat: add product

---
+++ app/frontend/biz/service/search_product.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+
+	product "github.com/MosesHe/gomall/app/frontend/hertz_gen/frontend/product"
+	"github.com/MosesHe/gomall/app/frontend/infra/rpc"
+	rpcproduct "github.com/MosesHe/gomall/rpc_gen/kitex_gen/product"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+)
+
+type SearchProductService struct {
+	RequestContext *app.RequestContext
+	Context        context.Context
+}
+
+func NewSearchProductService(Context context.Context, RequestContext *app.RequestContext) *SearchProductService {
+	return &SearchProductService{RequestContext: RequestContext, Context: Context}
+}
+
+func (h *SearchProductService) Run(req *product.SearchProductReq) (resp map[string]any, err error) {
+	products, err := rpc.ProductClient.SearchProducts(h.Context, &rpcproduct.SearchProductsReq{
+		Query: req.Q,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return utils.H{
+		"items": products.Results,
+		"q":     req.Q,
+	}, nil
+}
--- app/frontend/infra/rpc/client.go
+++ app/frontend/infra/rpc/client.go
@@ -5,19 +5,22 @@ import (
 
 	"github.com/MosesHe/gomall/app/frontend/conf"
 	frontendUtils "github.com/MosesHe/gomall/app/frontend/utils"
+	"github.com/MosesHe/gomall/rpc_gen/kitex_gen/product/productcatalogservice"
 	"github.com/MosesHe/gomall/rpc_gen/kitex_gen/user/userservice"
 	"github.com/cloudwego/kitex/client"
 	consul "github.com/kitex-contrib/registry-consul"
 )
 
 var (
-	UserClient userservice.Client
-	once       sync.Once
+	UserClient    userservice.Client
+	ProductClient productcatalogservice.Client
+	once          sync.Once
 )
 
 func Init() {
 	once.Do(func() {
 		initUserClient()
+		initProductClient()
 	})
 }
 
@@ -26,5 +29,13 @@ func initUserClient() {
 	frontendUtils.MustHandleError(err)
 
 	UserClient, err = userservice.NewClient("user", client.WithResolver(r))
+	frontendUtils.MustHandleError(err)
+}
+
+func initProductClient() {
+	r, err := consul.NewConsulResolver(conf.GetConf().Hertz.RegistryAddr)
+	frontendUtils.MustHandleError(err)
+
+	ProductClient, err = productcatalogservice.NewClient("product", client.WithResolver(r))
 	frontendUtils.MustHandleError(err)
 }
