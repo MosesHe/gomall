14e3b1927a33c317ae7c1698944f25d42c49ca3b
MosesHe <h394421599@gmail.com>
2025-03-01 21:23:37 +0800

    feat: update template

--- app/frontend/biz/service/home.go
+++ app/frontend/biz/service/home.go
@@ -4,7 +4,11 @@ import (
 	"context"
 
 	common "github.com/MosesHe/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/MosesHe/gomall/app/frontend/infra/rpc"
+	"github.com/MosesHe/gomall/rpc_gen/kitex_gen/product"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/kitex/pkg/klog"
 )
 
 type HomeService struct {
@@ -17,23 +21,13 @@ func NewHomeService(Context context.Context, RequestContext *app.RequestContext)
 }
 
 func (h *HomeService) Run(req *common.Empty) (map[string]any, error) {
-	//defer func() {
-	// hlog.CtxInfof(h.Context, "req = %+v", req)
-	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
-	//}()
-	// todo edit your code
-	resp := make(map[string]any)
-
-	items := []map[string]any{
-		{"Name": "T-shirt-1", "Price": 100, "Picture": "/static/image/T-shirt-1.jpg"},
-		{"Name": "T-shirt-2", "Price": 200, "Picture": "/static/image/T-shirt-1.jpg"},
-		{"Name": "T-shirt-3", "Price": 300, "Picture": "/static/image/T-shirt-1.jpg"},
-		{"Name": "T-shirt-4", "Price": 400, "Picture": "/static/image/T-shirt-1.jpg"},
-		{"Name": "T-shirt-5", "Price": 500, "Picture": "/static/image/T-shirt-1.jpg"},
-		{"Name": "T-shirt-6", "Price": 600, "Picture": "/static/image/T-shirt-1.jpg"},
+	ctx := h.Context
+	p, err := rpc.ProductClient.ListProducts(ctx, &product.ListProductsReq{})
+	if err != nil {
+		klog.Error(err)
 	}
-
-	resp["Title"] = "Hot Sales"
-	resp["Items"] = items
-	return resp, nil
+	return utils.H{
+		"title": "Hot sale",
+		"items": p.Products,
+	}, nil
 }
